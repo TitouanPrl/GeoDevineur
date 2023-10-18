ab5b00f326334fe2cbe5fcb17a56a9e188d67822
couziremi <couziremi@cy-tech.fr>
2023-10-18 17:30:50 +0200

    BASE V1.0

--- src/main/java/com/example/geodevineur/GeoDevineurApplication.java
+++ src/main/java/com/example/geodevineur/GeoDevineurApplication.java
@@ -9,7 +9,6 @@ public class GeoDevineurApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GeoDevineurApplication.class, args);
-        System.out.println("Coucou");
     }
 
 }
