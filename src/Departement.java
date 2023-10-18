5326bf8fcbc5f6701bb4ad4c2ce661a1ed954e10
MaelTriquet <mael1triquet@gmail.com>
2023-10-18 17:13:26 +0200

    departements

--- src/Departement.java
+++ src/Departement.java
@@ -1,22 +1,31 @@
 public class Departement {
-    private int id;
-    private String name;
-    private String mainCity;
+    private final String name;
+    private final int population;
+    private final int surface;
+    private final Cardinal cardinal;
+    private final boolean seaside;
+    private final int neightbours;
+    private final String id;
+    private final String politic;
+    private boolean possible = true;
+    private boolean potential = true;
 
-    public void setMainCity(String mainCity) {
-        this.mainCity = mainCity;
+    public Departement (String name_, int pop_, int surf_, Cardinal card_, boolean sea_, int neigh_, String id_, String politic_) {
+        name = name_;
+        population = pop_;
+        surface = surf_;
+        cardinal = card_;
+        seaside = sea_;
+        neightbours = neigh_;
+        id = id_;
+        politic = politic_;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setPossible(boolean possible) {
+        this.possible = possible;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public boolean checkVeracity() {
-        // Asks the database if the attributes match the id, return true if that's the case
-        return false;
+    public void setPotential(boolean potential) {
+        this.potential = potential;
     }
 }
