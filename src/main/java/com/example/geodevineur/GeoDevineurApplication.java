e51fafc1bb382bb7fb28fe156243cbcaeb2291a1
couziremi <couziremi@cy-tech.fr>
2023-10-18 16:50:47 +0200

    v1.1

---
+++ src/main/java/com/example/geodevineur/GeoDevineurApplication.java
@@ -0,0 +1,15 @@
+package com.example.geodevineur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoDevineurApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GeoDevineurApplication.class, args);
+        System.out.println("Coucou");
+    }
+
+}
