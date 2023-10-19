f8f9150f61df140041b251e1a65aac45bf92ace5
MaelTriquet <mael1triquet@gmail.com>
2023-10-19 12:53:50 +0200

    seasideCond

---
+++ src/SeasideCond.java
@@ -0,0 +1,15 @@
+public class SeasideCond extends Condition{
+    private boolean seaside;
+
+    public SeasideCond (Departement dep_) {
+        setAttributes(dep_);
+    }
+
+    protected void setAttributes(Departement dep) {
+        seaside = dep.getSeaside();
+    }
+
+    public boolean checksCondition(Departement dep) {
+        return dep.getSeaside() == seaside;
+    }
+}
