91f18c725c17b29b7c71ec90e515e8e847532696
TitouanPrl <titouan.pradal@gmail.com>
2023-10-20 16:07:28 +0200

    création table département

--- sql/init.sql
+++ sql/init.sql
@@ -12,3 +12,19 @@ CREATE TABLE REGION
 AS 
 SELECT * 
 FROM CSVREAD('../csv/regions.csv');
+
+CREATE TABLE DEPARTEMENT
+(
+  ID INT PRIMARY KEY,
+  Region VARCHAR(55),
+  Numero INT(2),
+  Habitants INT(55),
+  Superficie FLOAT(55),
+  Position VARCHAR(2),
+  Cotier BOOLEAN,
+  Voisins INT(2),
+  Politique VARCHAR(55)
+) 
+AS 
+SELECT * 
+FROM CSVREAD('../csv/regions.csv');
