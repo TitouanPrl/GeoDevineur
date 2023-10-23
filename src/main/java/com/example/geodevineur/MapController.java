e64c18af2460789d64827ba44707061eee3a9535
couziremi <couziremi@cy-tech.fr>
2023-10-23 17:55:30 +0200

    thymleaf working with html | page regles map interactive js (manque juste bdd pour noms departements

--- src/main/java/com/example/geodevineur/MapController.java
+++ src/main/java/com/example/geodevineur/MapController.java
@@ -3,19 +3,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+
 @Controller
 public class MapController{
-    @GetMapping("/apprendre-departements")
+    @GetMapping("apprendre-departements")
     public String apprendreDep(Model model) {
-        String numeroDep = "00";
-        //model add select pour chacun des departements de la bdd
-        String maVariableJava = "Ceci est une variable Java.";
-        model.addAttribute("maVariableJava", maVariableJava);
-        model.addAttribute("numeroDep", numeroDep);
-        return "apprendre-departements"; // renvoie le nom de votre fichier HTML
+        ArrayList<String> allDepartements = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 0; j <= 9; j++) {
+                allDepartements.add(String.format("%s%s", i, j));
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            allDepartements.remove(96);
+        }
+        allDepartements.remove(20);
+        allDepartements.add(20,"2B");
+        allDepartements.add(20,"2A");
+
+        StringBuilder selectContent = new StringBuilder();
+        selectContent.append("<select class=\"custom-select\" name=\"departements\" id=\"departement-select\">");
+        for (String option : allDepartements) {
+            selectContent.append("<option value=\"").append(option).append("\">").append(option).append("</option>");
+        }
+        selectContent.append("</select>");
+        model.addAttribute("selectContent", selectContent);
+        model.addAttribute("allDepartements", allDepartements);
+        return "apprendre-departements";
     }
 
-    @GetMapping("/apprendre-regions")
+    @GetMapping("apprendre-regions")
     public String apprendreReg(Model model) {
         String numeroDep = "00";
         //model add select pour chacun des departements de la bdd
