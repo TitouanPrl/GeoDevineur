50511286a4819cef40d72cfa3192dab57211a650
couziremi <couziremi@cy-tech.fr>
2023-10-27 08:35:43 +0200

    bdd fix + csv fix | problem remaining : population from csv

--- src/main/java/com/example/geodevineur/GeoDevineurApplication.java
+++ src/main/java/com/example/geodevineur/GeoDevineurApplication.java
@@ -3,8 +3,7 @@ package com.example.geodevineur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={
-"com.example.geodevineur.dep_reg", "com.example.geodevineur.enumerations", "com.example.geodevineur.DepartementRepository", "com.example.geodevineur.TableController"})
+@SpringBootApplication
 public class GeoDevineurApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/geodevineur/dep_reg/DepReg.java
+++ src/main/java/com/example/geodevineur/dep_reg/DepReg.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity 
+@Entity
 public abstract class DepReg {
     protected String name;
     @Id
