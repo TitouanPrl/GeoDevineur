e78be0d09c82447ad6edc31bef53e8f19c57b6cd
couziremi <couziremi@cy-tech.fr>
2023-11-01 15:06:05 +0100

    coordonnees v1

--- src/main/java/com/example/geodevineur/QuizzController.java
+++ src/main/java/com/example/geodevineur/QuizzController.java
@@ -1,8 +1,21 @@
 package com.example.geodevineur;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class QuizzController {
 
 
+    //private Time temps;
 
+
+//    public void playing(){
+//
+//        return "quizz";
+//    }
+//
+//    public void changeQuestion(String question){
+//
+//
+//    }
 
 }
